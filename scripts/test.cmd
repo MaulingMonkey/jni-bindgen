4ff40f827700d39ca3539f36b62c1dbc7983f651
MaulingMonkey <git@maulingmonkey.com>
2019-08-04 19:16:49 -0700

    test.cmd:  Test bindgen-jni (fast, catches early issues) before building android lib (slow).

--- scripts/test.cmd
+++ scripts/test.cmd
@@ -118,8 +118,8 @@
 :: Build
 
 @if /i not "%PLATFORM%" == "windows" goto :skip-windows
+    @call :try-cargo +%CHANNEL% test  %CARGO_FLAGS% --lib --all-features || goto :build-one-error
     @call :try-cargo +%CHANNEL% build %CARGO_FLAGS% --all --all-features || goto :build-one-error
-    @call :try-cargo +%CHANNEL% test  %CARGO_FLAGS% --all-features       || goto :build-one-error
     @call :try-cargo +%CHANNEL% doc --all-features --no-deps             || goto :build-one-error
     @goto :build-one-successful
     :: Notes on tested features:
