a203ad9259b98f8f277c6c5a78319b26b78dab43
MaulingMonkey <git@maulingmonkey.com>
2019-08-07 17:32:15 -0700

    preamble-contents:  Typo

--- jni-bindgen/src/emit_rust/preamble-contents.rs
+++ jni-bindgen/src/emit_rust/preamble-contents.rs
@@ -1,4 +1,4 @@
-// GENERATED WITH jni-bindgen, I DO NOT RECOMMEND EDITNIG THIS BY HAND
+// GENERATED WITH jni-bindgen, I DO NOT RECOMMEND EDITING THIS BY HAND
 
 #[macro_use(__jni_bindgen)] extern crate jni_glue;
 #[doc(hidden)] use jni_glue as __jni_bindgen;
