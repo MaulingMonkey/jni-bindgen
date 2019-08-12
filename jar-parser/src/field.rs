73991142d68615ea7e88b63ca9b3695e4d32eda5
MaulingMonkey <git@maulingmonkey.com>
2019-08-11 18:12:59 -0700

    Partial refactoring of JniField -> field::Descriptor
    - JniField -> field::Descriptor
    - JniBasicType -> field::BasicType
    - field::Field now checks descriptor validity on initial parse
    - JniField still exists for method descriptions for the moment, will be removed in a followup changelist.

--- jar-parser/src/field.rs
+++ jar-parser/src/field.rs
@@ -56,23 +56,27 @@ pub enum Constant {
 pub struct Field {
     pub flags:      Flags,
     pub name:       String,
-    pub descriptor: String,
+    descriptor:     String,
     pub deprecated: bool,
     pub constant:   Option<Constant>,
     _incomplete:    (),
 }
 
 impl Field {
-    pub fn new(flags: Flags, name: String, descriptor: String) -> Self {
-        Self {
+    pub fn new(flags: Flags, name: String, descriptor: String) -> io::Result<Self> {
+        Descriptor::from_str(descriptor.as_str())?;
+
+        Ok(Self {
             flags,
             name,
             descriptor,
             deprecated: false,
             constant: None,
             _incomplete: (),
-        }
+        })
     }
+
+    pub fn descriptor(&self) -> Descriptor { Descriptor::from_str(self.descriptor.as_str()).unwrap() } // Was already validated in Field::new / Field::read_one
 
     pub fn is_public(&self)     -> bool { self.flags.contains(Flags::PUBLIC) }
     pub fn is_private(&self)    -> bool { self.flags.contains(Flags::PRIVATE) }
@@ -98,6 +102,8 @@ impl Field {
         let name                = constants.get_utf8(read_u2(read)?)?.to_owned();
         let descriptor          = constants.get_utf8(read_u2(read)?)?.to_owned();
         let attributes_count    = read_u2(read)? as usize;
+
+        Descriptor::from_str(descriptor.as_str())?;
 
         let mut deprecated      = false;
         let mut constant        = None;
@@ -131,4 +137,108 @@ impl Field {
         }
         Ok(fields)
     }
+}
+
+
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub enum BasicType<'a> {
+    Byte,
+    Char,
+    Double,
+    Float,
+    Int,
+    Long,
+    Class(class::Id<'a>),
+    Short,
+    Boolean,
+    Void, // Only really crops up for method return types.
+}
+
+
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub enum Descriptor<'a> {
+    Single(BasicType<'a>),
+    Array { levels: usize, inner: BasicType<'a> },
+}
+
+impl<'a> Descriptor<'a> {
+    /// Consume a Descriptor from a string.  Will set `remaining` to parse the *remainder* of the string.
+    pub(crate) fn read_next(remaining: &mut &'a str) -> io::Result<Descriptor<'a>> {
+        let original = *remaining;
+        let mut array = 0;
+        let mut chars = remaining.chars();
+
+        let leaf = loop {
+            match chars.next() {
+                None => return io_data_err!("Expected basic type before end of string parsing field descriptor: {:?}", original),
+                Some('B') => { *remaining = chars.as_str(); break BasicType::Byte     }
+                Some('C') => { *remaining = chars.as_str(); break BasicType::Char     }
+                Some('D') => { *remaining = chars.as_str(); break BasicType::Double   }
+                Some('F') => { *remaining = chars.as_str(); break BasicType::Float    }
+                Some('I') => { *remaining = chars.as_str(); break BasicType::Int      }
+                Some('J') => { *remaining = chars.as_str(); break BasicType::Long     }
+                Some('L') => {
+                    let chars_str = chars.as_str();
+                    if let Some(semi) = chars_str.find(';') {
+                        *remaining = &chars_str[(semi+1)..];
+                        break BasicType::Class(class::Id(&chars_str[..semi]))
+                    } else {
+                        return io_data_err!("Expected ';' before end of string parsing field descriptor: {:?}", original)
+                    }
+                }
+                Some('S') => { *remaining = chars.as_str(); break BasicType::Short    }
+                Some('Z') => { *remaining = chars.as_str(); break BasicType::Boolean  }
+                Some('V') => { *remaining = chars.as_str(); break BasicType::Void     }
+                Some('[') => { array += 1; }
+                Some(ch)  => return io_data_err!("Unexpected character in field descriptor string: {:?}", ch),
+            }
+        };
+
+        match array {
+            0   => Ok(Descriptor::Single(leaf)),
+            n   => Ok(Descriptor::Array { levels: n, inner: leaf }),
+        }
+    }
+
+    pub(crate) fn from_str(field: &'a str) -> io::Result<Descriptor<'a>> {
+        let mut remaining = field;
+        let next = Self::read_next(&mut remaining)?;
+        if remaining.is_empty() {
+            Ok(next)
+        } else {
+            io_data_err!("Expected one field descriptor, got multiple.\n  Full field: {:?}\n  Unparsed: {:?}\n", field, remaining)
+        }
+    }
+}
+
+#[test] fn descriptor_from_str() {
+    // Single values
+    assert_eq!(Descriptor::from_str("F").unwrap(),                 Descriptor::Single(BasicType::Float));
+    assert_eq!(Descriptor::from_str("Ljava/foo/Bar;").unwrap(),    Descriptor::Single(BasicType::Class(class::Id("java/foo/Bar"))));
+
+    // Arrays
+    assert_eq!(Descriptor::from_str("[[F").unwrap(),               Descriptor::Array { levels: 2, inner: BasicType::Float });
+    assert_eq!(Descriptor::from_str("[[[Ljava/foo/Bar;").unwrap(), Descriptor::Array { levels: 3, inner: BasicType::Class(class::Id("java/foo/Bar")) });
+
+    // Erroneous input
+    assert!(Descriptor::from_str("").is_err());                               // No type
+    assert!(Descriptor::from_str("[[").is_err());                             // No type for array
+    assert!(Descriptor::from_str("Ljava/foo/Bar").is_err());                  // Missing semicolon
+    assert!(Descriptor::from_str("Ljava/foo/Bar;F").is_err());                // More after semicolon
+    assert!(Descriptor::from_str("Ljava/foo/Bar;Ljava/foo/Bar;").is_err());   // More after semicolon
+
+    // Multiple inputs
+    let mut class_float = "Ljava/foo/Bar;F";
+    assert_eq!(Descriptor::read_next(&mut class_float).unwrap(),    Descriptor::Single(BasicType::Class(class::Id("java/foo/Bar"))));
+    assert_eq!(Descriptor::read_next(&mut class_float).unwrap(),    Descriptor::Single(BasicType::Float));
+    assert_eq!(class_float, "");
+    assert!(   Descriptor::read_next(&mut class_float).is_err());
+
+    let mut class_class = "Ljava/foo/Bar;Ljava/foo/Bar;";
+    assert_eq!(Descriptor::read_next(&mut class_class).unwrap(),    Descriptor::Single(BasicType::Class(class::Id("java/foo/Bar"))));
+    assert_eq!(Descriptor::read_next(&mut class_class).unwrap(),    Descriptor::Single(BasicType::Class(class::Id("java/foo/Bar"))));
+    assert_eq!(class_class, "");
+    assert!(   Descriptor::read_next(&mut class_class).is_err());
 }
--- jni-bindgen/src/emit_rust/fields.rs
+++ jni-bindgen/src/emit_rust/fields.rs
@@ -1,6 +1,7 @@
 use super::*;
 
 use jar_parser::class;
+use jar_parser::field;
 
 use std::io;
 
@@ -21,11 +22,11 @@ impl<'a> Field<'a> {
         };
         if context.config.codegen.field_naming_style.const_finals {
             result.rust_const_value = java.constant.as_ref().map(|c| match c {
-                jar_parser::field::Constant::Double (value) => format!("{}", value),
-                jar_parser::field::Constant::Float  (value) => format!("{}", value),
-                jar_parser::field::Constant::Integer(value) => format!("{}", value),
-                jar_parser::field::Constant::Long   (value) => format!("{}", value),
-                jar_parser::field::Constant::String (value) => format!("{:?}", value),
+                field::Constant::Double (value) => format!("{}", value),
+                field::Constant::Float  (value) => format!("{}", value),
+                field::Constant::Integer(value) => format!("{}", value),
+                field::Constant::Long   (value) => format!("{}", value),
+                field::Constant::String (value) => format!("{:?}", value),
             });
         }
         result.set_mangling_style(&context.config.codegen.field_naming_style); // rust_name + mangling_style
@@ -53,30 +54,27 @@ impl<'a> Field<'a> {
             &self.java.name
         };
 
-        let rust_type = match JniField::from_str(self.java.descriptor.as_str()) {
-            Ok(JniField::Single(JniBasicType::Boolean)) => "bool",
-            Ok(JniField::Single(JniBasicType::Byte))    => "i8",
-            Ok(JniField::Single(JniBasicType::Char))    => "__jni_bindgen::jchar",
-            Ok(JniField::Single(JniBasicType::Double))  => "f64",
-            Ok(JniField::Single(JniBasicType::Float))   => "f32",
-            Ok(JniField::Single(JniBasicType::Int))     => "i32",
-            Ok(JniField::Single(JniBasicType::Long))    => "i64",
-            Ok(JniField::Single(JniBasicType::Short))   => "i16",
-            Ok(JniField::Single(JniBasicType::Class(class::Id("java/lang/String")))) if self.rust_const_value.is_some() => "&'static str",
-            Ok(JniField::Single(JniBasicType::Void)) => {
+        let descriptor = self.java.descriptor();
+        let rust_type = match descriptor {
+            field::Descriptor::Single(field::BasicType::Boolean) => "bool",
+            field::Descriptor::Single(field::BasicType::Byte)    => "i8",
+            field::Descriptor::Single(field::BasicType::Char)    => "__jni_bindgen::jchar",
+            field::Descriptor::Single(field::BasicType::Double)  => "f64",
+            field::Descriptor::Single(field::BasicType::Float)   => "f32",
+            field::Descriptor::Single(field::BasicType::Int)     => "i32",
+            field::Descriptor::Single(field::BasicType::Long)    => "i64",
+            field::Descriptor::Single(field::BasicType::Short)   => "i16",
+            field::Descriptor::Single(field::BasicType::Class(class::Id("java/lang/String"))) if self.rust_const_value.is_some() => "&'static str",
+            field::Descriptor::Single(field::BasicType::Void) => {
                 emit_reject_reasons.push("void is not a valid field type");
                 "()"
             },
-            Ok(JniField::Single(JniBasicType::Class(class))) => {
+            field::Descriptor::Single(field::BasicType::Class(class)) => {
                 emit_reject_reasons.push("Haven't yet implemented object field types");
                 class.as_str()
             },
-            Ok(JniField::Array { .. }) => {
+            field::Descriptor::Array { .. } => {
                 emit_reject_reasons.push("Haven't yet implemented array field types");
-                "???"
-            },
-            Err(_) => {
-                emit_reject_reasons.push("Failed to parse field type");
                 "???"
             },
         };
@@ -113,10 +111,10 @@ impl<'a> Field<'a> {
         writeln!(out, "{}/// {} {}", indent, &keywords, &self.java.name)?; // TODO: Field doc links
 
         if let Some(rust_const_value) = self.rust_const_value.as_ref() {
-            match rust_type {
-                "__jni_bindgen::jchar"  => writeln!(out, "{}{}pub const {} : {} = {}({});", indent, &attributes, rust_name, rust_type, rust_type, rust_const_value)?,
-                "bool"                  => writeln!(out, "{}{}pub const {} : {} = {};", indent, &attributes, rust_name, rust_type, if rust_const_value == "0" { "false" } else { "true" })?,
-                _                       => writeln!(out, "{}{}pub const {} : {} = {};", indent, &attributes, rust_name, rust_type, rust_const_value)?,
+            match descriptor {
+                field::Descriptor::Single(field::BasicType::Char)       => writeln!(out, "{}{}pub const {} : {} = {}({});", indent, &attributes, rust_name, rust_type, rust_type, rust_const_value)?,
+                field::Descriptor::Single(field::BasicType::Boolean)    => writeln!(out, "{}{}pub const {} : {} = {};", indent, &attributes, rust_name, rust_type, if rust_const_value == "0" { "false" } else { "true" })?,
+                _                                                       => writeln!(out, "{}{}pub const {} : {} = {};", indent, &attributes, rust_name, rust_type, rust_const_value)?,
             }
         } else if self.java.is_static() {
             writeln!(out, "{}{}pub fn {}<'env>(env: &'env Env) -> {} {{ ... }}", indent, &attributes, rust_getter_name, rust_type)?;
