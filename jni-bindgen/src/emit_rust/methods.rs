f1760a249c20d25ea07b0db905d1494c4c1b2069
MaulingMonkey <git@maulingmonkey.com>
2019-08-19 14:53:20 -0700

    Support array method arguments

--- jni-bindgen/src/emit_rust/methods.rs
+++ jni-bindgen/src/emit_rust/methods.rs
@@ -127,32 +127,48 @@ impl<'a> Method<'a> {
                         }
                     }
                 },
-                method::Type::Array { levels: 1, inner: method::BasicType::Void      } => {
-                    emit_reject_reasons.push("Accepting arrays of void isn't a thing");
-                    "&[()]".to_owned()
-                }
-                method::Type::Array { levels: 1, inner: method::BasicType::Boolean   } => { param_is_object = true; "impl __jni_bindgen::std::convert::Into<__jni_bindgen::std::option::Option<&'env __jni_bindgen::BooleanArray>>".to_owned() },
-                method::Type::Array { levels: 1, inner: method::BasicType::Byte      } => { param_is_object = true; "impl __jni_bindgen::std::convert::Into<__jni_bindgen::std::option::Option<&'env __jni_bindgen::ByteArray   >>".to_owned() },
-                method::Type::Array { levels: 1, inner: method::BasicType::Char      } => { param_is_object = true; "impl __jni_bindgen::std::convert::Into<__jni_bindgen::std::option::Option<&'env __jni_bindgen::CharArray   >>".to_owned() },
-                method::Type::Array { levels: 1, inner: method::BasicType::Short     } => { param_is_object = true; "impl __jni_bindgen::std::convert::Into<__jni_bindgen::std::option::Option<&'env __jni_bindgen::ShortArray  >>".to_owned() },
-                method::Type::Array { levels: 1, inner: method::BasicType::Int       } => { param_is_object = true; "impl __jni_bindgen::std::convert::Into<__jni_bindgen::std::option::Option<&'env __jni_bindgen::IntArray    >>".to_owned() },
-                method::Type::Array { levels: 1, inner: method::BasicType::Long      } => { param_is_object = true; "impl __jni_bindgen::std::convert::Into<__jni_bindgen::std::option::Option<&'env __jni_bindgen::LongArray   >>".to_owned() },
-                method::Type::Array { levels: 1, inner: method::BasicType::Float     } => { param_is_object = true; "impl __jni_bindgen::std::convert::Into<__jni_bindgen::std::option::Option<&'env __jni_bindgen::FloatArray  >>".to_owned() },
-                method::Type::Array { levels: 1, inner: method::BasicType::Double    } => { param_is_object = true; "impl __jni_bindgen::std::convert::Into<__jni_bindgen::std::option::Option<&'env __jni_bindgen::DoubleArray >>".to_owned() },
-                method::Type::Array { levels: 1, inner: method::BasicType::Class(class) } => {
-                    if let Ok(feature) = Struct::feature_for(context, class) {
-                        required_features.insert(feature);
-                    } else {
-                        emit_reject_reasons.push("ERROR:  Unable to resolve class feature");
+                method::Type::Array { levels, inner } => {
+                    let mut buffer = "impl __jni_bindgen::std::convert::Into<__jni_bindgen::std::option::Option<&'env ".to_owned();
+                    for _ in 0..(levels-1) {
+                        buffer.push_str("__jni_bindgen::ObjectArray<");
                     }
+                    match inner {
+                        method::BasicType::Boolean      => buffer.push_str("__jni_bindgen::BooleanArray"),
+                        method::BasicType::Byte         => buffer.push_str("__jni_bindgen::ByteArray"),
+                        method::BasicType::Char         => buffer.push_str("__jni_bindgen::CharArray"),
+                        method::BasicType::Short        => buffer.push_str("__jni_bindgen::ShortArray"),
+                        method::BasicType::Int          => buffer.push_str("__jni_bindgen::IntArray"),
+                        method::BasicType::Long         => buffer.push_str("__jni_bindgen::LongArray"),
+                        method::BasicType::Float        => buffer.push_str("__jni_bindgen::FloatArray"),
+                        method::BasicType::Double       => buffer.push_str("__jni_bindgen::DoubleArray"),
+                        method::BasicType::Class(class) => {
+                            if let Ok(feature) = Struct::feature_for(context, class) {
+                                required_features.insert(feature);
+                            } else {
+                                emit_reject_reasons.push("ERROR:  Unable to resolve class feature");
+                            }
+                            buffer.push_str("__jni_bindgen::ObjectArray<");
+                            match context.java_to_rust_path(class) {
+                                Ok(path) => buffer.push_str(path.as_str()),
+                                Err(_) => {
+                                    emit_reject_reasons.push("ERROR:  Failed to resolve JNI path to Rust path for argument type");
+                                    buffer.push_str("???");
+                                }
+                            }
+                            buffer.push_str(">");
+                        },
+                        method::BasicType::Void => {
+                            emit_reject_reasons.push("ERROR:  Arrays of void isn't a thing");
+                            buffer.push_str("[()]");
+                        },
+                    }
+                    for _ in 0..(levels-1) {
+                        buffer.push_str(">"); // ObjectArray s
+                    }
+                    buffer.push_str(">>"); // Option, Into
+
                     param_is_object = true;
-                    emit_reject_reasons.push("ERROR:  Passing in arrays of objects is not yet supported");
-                    format!("{:?}", arg)
-                },
-                method::Type::Array { .. } => {
-                    param_is_object = true;
-                    emit_reject_reasons.push("ERROR:  Passing in arrays of arrays is not yet supported");
-                    format!("{:?}", arg)
+                    buffer
                 },
             };
 
